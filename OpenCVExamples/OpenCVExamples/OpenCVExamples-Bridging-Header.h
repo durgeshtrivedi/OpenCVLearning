344765c7cb96f184fa1bf9bab42d457d44edf6c0
Durgesh Trivedi <durgesh.trivedi@synerzip.com>
2017-02-10 15:54:50 +0530

    added learn opencv  example code https://github.com/spmallick/learnopencv

---
+++ OpenCVExamples/OpenCVExamples/OpenCVExamples-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  OpenCVExamples-Bridging-Header.h
+//  OpenCVExamples
+//
+//  Created by Durgesh Trivedi on 14/11/16.
+//  Copyright © 2016 durgesh. All rights reserved.
+//
+
+#ifndef OpenCVExamples_Bridging_Header_h
+#define OpenCVExamples_Bridging_Header_h
+
+//YourProjectName-Bridging-Header#include "CV"
+
+#endif /* OpenCVExamples_Bridging_Header_h */
